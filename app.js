f0a39968164b48a455b6220281b01dd33dfd7953
daniella birch <daniellabirch@gmail.com>
2017-06-06 16:53:43 -0600

    created & served html file
    I got the html file to serve!

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var http = require('http');
+var fs = require("fs");
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
 var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-  req.end('Hello World\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    }
+	else{
+      res.writeHead(200,{
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
