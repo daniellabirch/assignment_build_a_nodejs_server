c73dad305bbd30385612d401188883dcdb931c3c
daniella birch <daniellabirch@gmail.com>
2017-06-21 13:10:56 -0600

    req and res objects displayed
    took me awhile to get working but I think I’m starting to get a hang of
    how node displays the page

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ var http = require('http');
 var fs = require("fs");
 
 var hostname = '127.0.0.1';
-var port = 3000;
+var port = 3000; 
 
 var server = http.createServer((req, res) => {
   fs.readFile('./public/index.html', 'utf8', function(err, data) {
+	var reqJSON = {
+    	url:req.url,
+    	method:req.method,
+    	httpVersion:req.httpVersion,
+    	headers:req.headers
+	};
+	  
+	var resJSON = {
+		statusMessage:res.statusMessage,
+    	statusCode:res.statusCode,
+    	_header:res._header
+	};
+	  
+	reqJSON = JSON.stringify(reqJSON, null, 2);
+	resJSON = JSON.stringify(resJSON, null, 2);
+	  
+	data=data.replace("{{req}}",reqJSON);
+	data=data.replace("{{res}}",resJSON);
+	  
     if (err) {
       res.writeHead(404);
       res.end("404 Not Found");
